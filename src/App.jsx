5c5b8e588a7c397030378e423cf73a0d1b74585a
eduardolu <edu291086@gmail.com>
2023-02-17 18:59:27 +0100

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+// import {} from ''
+
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div className='container mx-auto'>
+      {/* <navbar /> */}
+      <h1 className='text-9xl text-white text-center'>Nevera</h1>
+    </div>
+  )
+}
+
